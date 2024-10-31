b57ef3927f94726e6b12617d5fb87acd741e1230
hyenee99 <twoddal2@naver.com>
2024-10-31 23:49:32 +0900

    투두리스트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './Todolist';
+import Clock from './Timer';
 
 function App() {
-  let name ="리액트";
-  const style = {
-    backgroundColor : 'black',
-    color: 'white',
-    fontSize : '48px',
-    fontWeight : 'bold',
-    padding: '20px'
-  }
-
   return (
-    <div style = {style}>
-      <h1 className='test'>Hello,
-      {
-      name ==='리액트' ? (<h1>YES</h1>) : (<h1>No</h1>)
-      } !! </h1>
-      <p>반갑습니다.</p>
+    <div className='container'>
+      <TodoList></TodoList>
+      <Clock></Clock>
     </div>
   );
 }
