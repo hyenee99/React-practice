f673e257b5b746d1880b59e29e566d863e23cebd
hyenee99 <twoddal2@naver.com>
2024-11-01 19:14:34 +0900

    모달 대화상자를 이용해 todo 상세정보 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './Todolist';
 import Clock from './Timer';
+import MyWeather from './MyWeather';
 
 function App() {
   return (
     <div className='container'>
       <TodoList></TodoList>
       <Clock></Clock>
+      <MyWeather weather='흐림'>일기예보</MyWeather>
     </div>
   );
 }
